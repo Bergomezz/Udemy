aa80cf21e8a4b527b8634a362d52df525e123a4f
Bergomezz <bernardogomezz@yahoo.com.br>
2022-04-11 14:21:34 -0300

    Curso de boas praticas finalizado

--- CypressBoasPraticas/cypress/integration/sensitive-data/sample.spec.js
+++ CypressBoasPraticas/cypress/integration/sensitive-data/sample.spec.js
@@ -4,7 +4,6 @@ describe('Sensitive data bad practice', () => {
   })
 
   it('fills the form with sensitive data', () => {
-    cy.get('#email').type('joe@example.com')
-    cy.get('#password').type('s3Cr€7-p@s5w0rd')
+    cy.loginUser()
   })
 })
--- CypressBoasPraticas/cypress/support/commands.js
+++ CypressBoasPraticas/cypress/support/commands.js
@@ -11,3 +11,18 @@ Cypress.Commands.add('assertResults', () => {
   })
 })
 
+Cypress.Commands.add('updateDestination', data => {
+    cy.get('#destination_name')
+      .clear()
+      .type(data.name)
+    cy.get('#destination_description')
+      .clear()
+      .type(data.description)
+    cy.get('input[type="submit"]')
+      .click()
+})
+
+Cypress.Commands.add('loginUser', () => {
+    cy.get('#email').type(Cypress.env('user_email'))
+    cy.get('#password').type(Cypress.env('user_password'), { log: false })
+})
