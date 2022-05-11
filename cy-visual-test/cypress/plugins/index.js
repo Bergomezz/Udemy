c1ea0da963262c9aae2f0354aca5953b2ca37385
Bergomezz <bernardogomezz@yahoo.com.br>
2022-05-10 21:43:28 -0300

    complete configuration of snapshor image plugin

---
+++ cy-visual-test/cypress/plugins/index.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
+
+module.exports = (on, config) => {
+	addMatchImageSnapshotPlugin(on, config)
+}
