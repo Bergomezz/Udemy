f6216bfdb8fceced6057e252254e17f860430a52
Bergomezz <bernardogomezz@yahoo.com.br>
2022-05-24 12:04:20 -0300

    Testing some code in the api

--- Cypres-API/cypress/integration/test.spec.js
+++ Cypres-API/cypress/integration/test.spec.js
@@ -1,15 +1,20 @@
 describe('REST API test with Cypress', () => {
 	it('API test - validate headers', () => {
-		cy.request({
-			method: 'POST',
-			url: 'https://7eb984w4j4.execute-api.us-east-1.amazonaws.com/dev/lambdastresstest',
-			failOnStatusCode: false,
-			Body: {
-				message: 'testing something',
-				number: 123456789,
-			},
-		}).then(response => {
-			cy.writeFile('cypress/json/user.json', response.body)
-		})
+		cy.intercept(
+			'POST',
+			'https://7eb984w4j4.execute-api.us-east-1.amazonaws.com/dev/lambdastresstest',
+			{
+				failOnStatusCode: false,
+				body: {
+					message: 'testing something',
+					number: 123456789,
+				},
+				statusCode: 404,
+			}
+		)
+			.as('postInformation')
+			.should(response => {
+				cy.writeFile('cypress/fixtures/user.json', response.body)
+			})
 	})
 })
